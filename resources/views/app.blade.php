0eba3564cb0624396677247f0de4b82c150854a9
MahaCreative <gunturmadjid.3@gmail.com>
2025-12-29 03:32:32 +0800

    'update'

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,24 @@
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+    {{-- Default SEO metas (can be overridden by page via Inertia Head) --}}
+    <meta name="description" content="{{ $page['props']['seo']['description'] ?? config('app.description', '') }}">
+    <meta name="keywords" content="{{ $page['props']['seo']['keywords'] ?? '' }}">
+    <link rel="canonical" href="{{ $page['props']['seo']['canonical'] ?? url()->current() }}">
+
+    {{-- Open Graph / Facebook --}}
+    <meta property="og:title" content="{{ $page['props']['seo']['title'] ?? config('app.name') }}" />
+    <meta property="og:description" content="{{ $page['props']['seo']['description'] ?? config('app.description', '') }}" />
+    <meta property="og:image" content="{{ $page['props']['seo']['image'] ?? asset('image/PT-DHERVA-INVESTINDO-14-1-20258.png') }}" />
+    <meta property="og:type" content="{{ $page['props']['seo']['type'] ?? 'website' }}" />
+    <meta property="og:url" content="{{ $page['props']['seo']['canonical'] ?? url()->current() }}" />
+
+    {{-- Twitter Card --}}
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="{{ $page['props']['seo']['title'] ?? config('app.name') }}" />
+    <meta name="twitter:description" content="{{ $page['props']['seo']['description'] ?? config('app.description', '') }}" />
+    <meta name="twitter:image" content="{{ $page['props']['seo']['image'] ?? asset('image/PT-DHERVA-INVESTINDO-14-1-20258.png') }}" />
+
     <link rel="icon" href="image/favicon.ico" sizes="any">
 
     <link rel="apple-touch-icon" href="image/apple-touch-icon.png">
